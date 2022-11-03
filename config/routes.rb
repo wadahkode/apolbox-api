d8e3793362de776163f3a7c5f72c21b922b3eede
wadahkode <mvp.dedefilaras@gmail.com>
2022-11-04 01:05:12 +0700

    update

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  namespace :api do
-    namespace :v1 do
-      resources :users
-    end
-  end
-end
